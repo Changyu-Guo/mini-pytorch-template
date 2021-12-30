b9810370898665162d5dd021e2646508c2639465
Changyu-Guo <1028677200@qq.com>
2021-12-30 13:34:55 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf - 8 -*-
+
+import argparse
+
+from args import get_parser
+from trainers import get_trainer
+import utils
+
+
+def main(mode):
+    parser = get_parser(mode)
+    args, _ = parser.parse_known_args()
+    configs = utils.get_configs(args)
+    trainer_cls = get_trainer(configs.task.trainer)
+    trainer = trainer_cls(configs)
+    trainer.run(mode)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'mode', type=str, choices=['train', 'eval', 'predict'],
+        help='Mode: `train`, `eval` or `predict`'
+    )
+    args, _ = parser.parse_known_args()
+    main(args.mode)
