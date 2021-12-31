be74c9ddf28d80aabec35ed4daace234786766e0
Changyu-Guo <1028677200@qq.com>
2021-12-31 10:22:17 +0800

    add args.py

---
+++ args.py
@@ -0,0 +1,40 @@
+# -*- coding: utf - 8 -*-
+
+import argparse
+
+
+def _add_common_args(parser):
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--device', type=str, required=True)
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--debug', action='store_true')
+    return parser
+
+
+def train_parser():
+    parser = argparse.ArgumentParser()
+    _add_common_args(parser)
+    return parser
+
+
+def eval_parser():
+    parser = argparse.ArgumentParser()
+    _add_common_args(parser)
+    return parser
+
+
+def predict_parser():
+    parser = argparse.ArgumentParser()
+    _add_common_args(parser)
+    return parser
+
+
+PARSER_MAPPINGS = {
+    'train': train_parser,
+    'eval': eval_parser,
+    'predict': predict_parser
+}
+
+
+def get_parser(mode):
+    return PARSER_MAPPINGS[mode]()
