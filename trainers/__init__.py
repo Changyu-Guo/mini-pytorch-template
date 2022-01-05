6f0b5818f1f03f67fb10d4fe1ae5c19ccc24d830
Changyu-Guo <1028677200@qq.com>
2022-01-05 17:24:35 +0800

    feat: add get_trainer_cls func

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,1 +1,13 @@
 # -*- coding: utf - 8 -*-
+
+from trainers.gpu_trainer import GPUTrainer
+from trainers.ddp_trainer import DDPTrainer
+
+TRAINER_MAPPINGS = {
+    'gpu_trainer': GPUTrainer,
+    'ddp_trainer': DDPTrainer
+}
+
+
+def get_trainer_cls(name):
+    return TRAINER_MAPPINGS[name]
